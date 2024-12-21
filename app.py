f228ed1076d56609c53ecc1df8c3f8d1e5c74c8e
LINDA-SUN\linda <rachelsun5@gmail.com>
2024-12-22 02:01:10 +0800

    login page, part of customer back, without DB

--- app.py
+++ app.py
@@ -1,152 +1,118 @@
-from flask import Flask, render_template, request, session, redirect
 from functools import wraps
-from tkinter import messagebox
-from dbUtils import product, login, my_product, select_product, add_price, get_record, add_product, edit, delete
+from flask import Flask, redirect, render_template, request, session
+import dbUtils
 
-app = Flask(__name__, static_folder='templates', static_url_path='/')
+app = Flask(__name__)
 app.config['SECRET_KEY'] = '123TyU%^&'
 
+url_role_map = {
+    1: "/store",
+    2: "/customer",
+    3: "/delivery",
+}
 
 def login_required(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
 
         if not session.get('loginID'):
-            return redirect('/loginPage')
+            return redirect('/login')
         return f(*args, **kwargs)
-    
+
     return wrapper
 
 
-@app.route('/loginPage', methods=['GET', 'POST']) #登入頁面
-def login_page():
-    if request.method == 'POST':
+def role_check(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
 
-        form = request.form
-        account = form.get('account')
-        password = form.get('PWD')
+        if not request.path.startswith(url_role_map[session.get('role')]) :
+            return redirect('/login')
+        
+        return f(*args, **kwargs)
 
-        user = login(account, password)  
-        if user:
-            user_info = user[0]
-            session['loginID'] = user_info['account']
-            session['UserName'] = user_info['UserName']
-            session['UID'] = user_info['UID']
-            return redirect("/Home")
-        else:
-            return render_template('loginPage.html', error = '帳號或密碼錯誤')
+    return wrapper
 
-    # 若是 GET 請求，直接出現登入頁面
-    return render_template('loginPage.html')
-
-
-
-@app.route('/logout') #登出
-def logout():
-
-    session.clear()  # 清除所有 session 資料
-
-    return redirect('/loginPage')
-
-
-
-@app.route('/Home') #首頁
+# 預設情況下，Flask 會使用 "./static" 資料夾作為靜態資源
+# page
+@app.route('/')
 @login_required
 def home():
+    return render_template('index.html')
 
-    user_name = session.get('UserName', ' ') # 將資料從 session 抓出來，沒有資料就顯示空白
-    product_list = product()
-
-    return render_template('HomePage.html', userName=user_name, products=product_list)
-
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html')
 
 
-@app.route('/bidding/<int:pid>', methods=['GET','POST']) # 出價
+@app.route('/store', methods=['GET'])
 @login_required
-def Bid(pid):
-
-    user_name = session.get('UserName')
-    product_info = select_product(pid) # 單一商品的資訊
-    
-    if request.method == 'POST':
-        BidPrice = int(request.form['bid_price']) # 從 form get 出價
-        UID = session.get('UID') # 出價的用戶
-    
-        add_price(pid, BidPrice, UID)
-        return redirect('/Home')
-    
-    
-    #add_price = int(request.form['add_price'])
-    return render_template('BidPage.html', userName=user_name, product_i=product_info)
+@role_check
+def store():
+    return render_template('store.html')
 
 
-@app.route('/add_product', methods = ['GET','POST']) #新增商品
+@app.route('/customer', methods=['GET'])
 @login_required
-def AddProduct():
+@role_check
+def customer():
+    return render_template('customer.html')
 
-    user_name = session.get('UserName')
-
-    if request.method=='POST':
-        ProductName = str(request.form['P_name'])
-        Introduce = str(request.form['P_introduce'])
-        StartPrice = int(request.form['P_price'])
-        UID = session.get('UID')
-        add_product(ProductName, Introduce, StartPrice, UID)
-        return redirect('/self_page')
-    
-    return render_template('AddPage.html', userName=user_name)
-    
-
-
-@app.route('/self_page', methods=['GET','POST']) #個人頁
+@app.route('/customer/store/<int:store_id>', methods=['GET'])
 @login_required
-def self():
-
-    user_name = session.get('UserName', ' ')
-    UID = session.get('UID')
-    MP=my_product(UID) #商品
-
-    if request.method=='POST': #刪除商品
-        pid = int(request.form['pid'])
-        delete(pid)
-        return redirect('/self_page')
-   
+@role_check
+def customer_stroe(store_id):
+    print(store_id)
     
-    return render_template('SelfPage.html', userName=user_name, myProduct=MP)
+    return render_template('customer_store.html')
 
 
-
-@app.route('/record_page/<int:pid>', methods=['GET']) #出價記錄
+@app.route('/delivery', methods=['GET'])
 @login_required
-def record(pid):
-    user_name = session.get('UserName', ' ')
-    p_record=get_record(pid) #商品出價記錄
-    return render_template('RecordPage.html', userName=user_name, Record=p_record)
+@role_check
+def delivery():
+    return render_template('delivery.html')
 
 
-
-
+# api
+@app.route('/login', methods=['POST'])
+def api_login():
     
-
-@app.route('/edit_product/<int:pid>', methods=['GET','POST']) #修改商品
-@login_required
-def Edit(pid):
-    
-    user_name = session.get('UserName')
-    PI = select_product(pid)
-
-    if request.method == 'POST':
-        ProductName = str(request.form['P_name'])
-        Introduce = str(request.form['P_introduce'])
-        StartPrice = int(request.form['P_price']) 
-        pid = int(request.form['pid'])
-        edit(ProductName, Introduce, StartPrice, pid)
-        return redirect('/self_page')
-    
-    return render_template('EditPage.html', userName=user_name, product_i = PI)
+    form = request.json
+    username = form.get('username')
+    password = form.get('password')
+    user = dbUtils.login(username, password)
+    if user :
+        user_info = user[0]
+        session['loginID'] = user_info['username']
+        session['role'] = user_info['role']
+        session['id'] = user_info['id']
+        return {"url": url_role_map[user_info['role']]}
+    return {"url": "/login"}
 
 
+@app.route('/store-list', methods=['GET'])
+def api_store_list():
+    store_list = dbUtils.get_store_list()
+    return {"data": store_list}
+
+@app.route('/store-list/<int:store_id>/menu', methods=['GET'])
+def api_store_menu(store_id):
+    store_menu = dbUtils.get_store_menu(store_id)
+    return {"data": store_menu}
+
+@app.route('/store-list/<int:store_id>/order', methods=['POST'])
+def api_store_order(store_id):
+    # 品項、數量、單價
+    # 先撈出 customer_id
+    print(session['id'])
+    customer_id = dbUtils.get_customer_id(session['id'])[0]["id"]
+    print(customer_id)
+    # 再寫入 customer_order
+
+    customer_order = dbUtils.add_customer_order(customer_id, store_id)
+    return 
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
--- dbUtils.py
+++ dbUtils.py
@@ -1,186 +1,64 @@
-from flask import Flask, render_template, request, session, redirect
-from functools import wraps
 #!/usr/local/bin/python
 # Connect to MariaDB Platform
 import mysql.connector #mariadb
 
 try:
-	#連線DB
-	conn = mysql.connector.connect(
-		user="root",
-		password="",
-		host="localhost",
-		port=3306,
-		database="bidplatform"
-	)
-	#建立執行SQL指令用之cursor, 設定傳回dictionary型態的查詢結果 [{'欄位名':值, ...}, ...]
-	cursor=conn.cursor(dictionary=True)
+    #連線DB
+    conn = mysql.connector.connect(
+        user="root",
+        password="",
+        host="localhost",
+        port=3306,
+        database="food"
+    )
+    #建立執行SQL指令用之cursor, 設定傳回dictionary型態的查詢結果 [{'欄位名':值, ...}, ...]
+    cursor=conn.cursor(dictionary=True)
 except mysql.connector.Error as e: # mariadb.Error as e:
-	print(e)
-	print("Error connecting to DB")
-	exit(1)
+    print(e)
+    print("Error connecting to DB")
+    exit(1)
 
 
 #登入
 
-def login(account, password):
+def login(username, password):
     """驗證用戶的帳號和密碼，返回用戶資訊或 None。"""
-    sql = "SELECT * FROM `user` WHERE account = %s AND password = %s"
-    param=(account, password)
+    sql = "SELECT * FROM `user` WHERE username = %s AND password = %s"
+    param=(username, password)
     cursor.execute(sql,param)
     return cursor.fetchall()
 
-
-#新增資料
-
-def add_price(pid, bid_price, UID):  # 用戶幫商品加價
-	
-	sql="INSERT INTO record (pid, BidPrice, UID) VALUES (%s, %s, %s)"
-	param=(pid, bid_price, UID)
-	cursor.execute(sql,param)
-	conn.commit()
-	return
-
-def add_product(ProductName, Introduce, StartPrice, UID):  # 新增商品
-	sql='''
-	insert into 
-	product(ProductName, Introduce, StartPrice, UID) 
-	values (%s, %s, %s, %s);'''
-	param=(ProductName, Introduce, StartPrice, UID)
-	cursor.execute(sql, param)
-	conn.commit()
-	return 
-
-#刪除跟修改
-	
-def delete(pid):  #刪除商品
-	sql='''
-	DELETE product, record
-    FROM product
-    LEFT JOIN record ON product.pid = record.pid
-    WHERE product.pid = %s;'''
-	
-	cursor.execute(sql, (pid,))
-	conn.commit()
-	return
-
-def edit(ProductName, Introduce, StartPrice, pid):  #修改商品資料
-	sql='''
-	update `product` 
-	set 
-		ProductName = %s, 
-		Introduce = %s, 
-		StartPrice = %s 
-	where pid = %s;'''
-	param=(ProductName, Introduce, StartPrice, pid)
-	cursor.execute(sql,param)
-	conn.commit()
-	return
-	
-
-#找資料
-
-
-def product(): # 找全部的商品
-    sql="""
-	SELECT 
-		product.pid,
-		product.ProductName, 
-		product.Introduce, 
-		product.StartPrice,
-		product.StartPrice , 
-		user.UserName, 
-		product.AddTime,
-		greatest(ifnull(max(record.BidPrice), product.StartPrice), product.StartPrice) as price
-	FROM `product` 
-	LEFT JOIN user ON user.UID = product.UID
-	left join record on record.pid = product.pid
-	group by product.pid;"""
-    
+def get_store_list() :
+    sql = "SELECT * FROM `store`"
     cursor.execute(sql)
     return cursor.fetchall()
 
-def my_product(UID):  # 找 user 的商品（首頁）
-	sql="""
-	SELECT 
-		user.UID, 
-		product.pid,
-		product.ProductName, 
-		product.Introduce, 
-		product.StartPrice,
-		product.AddTime,
-		product.StartPrice, 
-		greatest(ifnull(max(record.BidPrice), product.StartPrice), product.StartPrice) as price
-	from `product`
-	left join `record` on product.pid=record.pid
-	left join `user` on product.UID=user.UID 
-	where product.UID=%s
-	group by product.pid;"""
-	param = (UID,)
-	cursor.execute(sql, param)
-	return cursor.fetchall()
+def get_store_menu(sid) :
+    sql = "SELECT name FROM `store_menu` where sid = %s"
+    param = [sid]
+    cursor.execute(sql,param)
+    
+    return cursor.fetchall()
 
-def select_product(pid): # 找單一商品（用在出價、修改）
-	sql="""
-	SELECT 
-		product.pid, 
-		product.ProductName, 
-		product.Introduce, 
-		product.StartPrice, 
-		greatest(ifnull(max(record.BidPrice), product.StartPrice), product.StartPrice) as price, 
-		record.BidPrice,
-		user.UserName, 
-		product.AddTime
-	from `product` 
-	left join `record` on record.pid=product.pid
-	left join `user` on product.UID=user.UID 
-	where product.pid=%s 
-	group by product.pid"""
-	param = (pid,)
-	cursor.execute(sql, param)
-	return cursor.fetchall()
+def get_customer_id(id) :
+    print('============')
+    print(id)
+    sql = "SELECT id FROM `customer` where uid = %s"
+    param = [id]
+    cursor.execute(sql,param)
+    
+    return cursor.fetchall()
 
-def get_record(pid): #找出價記錄
-	sql="""
-	SELECT 
-		product.pid, 
-		record.UID,
-		product.ProductName, 
-		product.Introduce, 
-		product.StartPrice, 
-		greatest(ifnull(max(record.BidPrice), product.StartPrice), product.StartPrice) as NowPrice, 
-		record.BidPrice,
-		NP.NowPrice,
-		record.BidTime,
-		sell.UserName as S_user,
-		bid.UserName as B_user,
-		product.AddTime,
-		record.BidPrice - ifnull(lag(record.BidPrice) over (order by rid), product.StartPrice) as addPrice
-	from `product` 
-		LEFT JOIN (
-    	SELECT 
-        	product.pid,
-        	greatest(ifnull(max(record.BidPrice), product.StartPrice), product.StartPrice) AS NowPrice
-    	FROM `product`
-    		LEFT JOIN `record` ON record.pid = product.pid
-    	GROUP BY product.pid
-			) AS NP ON NP.pid = product.pid
-		left join `record` on record.pid = product.pid
-		left join `user` as `sell` on sell.UID = product.UID 
-		left join `user` as `bid` on record.UID = bid.UID
-	where product.pid=%s 
-	
-	group by 
-		product.pid,
-		record.BidTime
-	
-	order by 
-		record.BidPrice
-	desc;"""
-	# left join `user` as `sell` on sell.UID = product.UID 找賣家資料
-	# left join `user` as `bid` on record.UID = bid.UID 找出價者資料
-	param = (pid,)
-	cursor.execute(sql, param)
-	return cursor.fetchall()
+def add_customer_order(id, store_id) :
+    sql = """
+    INSERT INTO
+      `customer_order`
+      (`customer_id`, `store_id`, `delivery_id`) 
+      VALUES ( %s,%s,%s)"""
+    param = (id,store_id,-1)
+    cursor.execute(sql,param)
+    conn.commit()
+    return 
+
 
 
